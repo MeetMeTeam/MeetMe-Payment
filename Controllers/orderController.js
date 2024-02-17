fa66ccfe6fdfb0b46ebf1c94d35562b6139653dc
Nuttawat Rodsomboob <nuttawat.rod@innovasive.co.th>
2024-02-17 17:56:23 +0700

    update validate

--- Controllers/orderController.js
+++ Controllers/orderController.js
@@ -8,68 +8,85 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 const CheckOut = async (req, res) => {
   const { user, product } = req.body;
-  try {
-    // create payment session
-    const orderId = uuidv4();
-    const session = await stripe.checkout.sessions.create({
-      payment_method_types: ["card"],
-      line_items: [
-        {
-          price_data: {
-            currency: "thb",
-            product_data: {
-              name: product.name,
-            },
-            unit_amount: product.price * 100,
-          },
-          quantity: product.quantity,
-        },
-      ],
-      mode: "payment",
-      success_url: `${process.env.URL_WEB}/payment-success/${orderId}`,
-      cancel_url: `${process.env.URL_WEB}/payment-cancel/${orderId}`,
-    });
-    const orderData = {
-      user_id: user.userId,
-      session_id: session.id,
-      status: session.status,
-      order_id: orderId,
-      url: session.url,
-    };
-    const newOrder = new Order(orderData);
-    newOrder
-      .save()
-      .then((savedOrder) => {
-        res.json({
-          message: "Checkout success.",
-          id: session.id,
-          url: session.url,
-        });
-      })
-      .catch((error) => {
-        console.error("Error saving order:", error);
-        res.status(500).json({
-          message: "Error saving order",
-          error: error.message,
-        });
-      });
-  } catch (error) {
-    console.error("Error creating user:", error.message);
-    res.status(400).json({ error: "Error payment" });
+  if (!(user && product)) {
+    res.status(400).json({ message: "need user and product data" });
+  } else {
+    try {
+      const order = await Order.find({ user_id: user.userId });
+      if (order.length > 0) {
+        try {
+          // create payment session
+          const orderId = uuidv4();
+          const session = await stripe.checkout.sessions.create({
+            payment_method_types: ["card"],
+            line_items: [
+              {
+                price_data: {
+                  currency: "thb",
+                  product_data: {
+                    name: product.name,
+                  },
+                  unit_amount: product.price * 100,
+                },
+                quantity: product.quantity,
+              },
+            ],
+            mode: "payment",
+            success_url: `${process.env.URL_WEB}/payment-success/${orderId}`,
+            cancel_url: `${process.env.URL_WEB}/payment-cancel/${orderId}`,
+          });
+          const orderData = {
+            user_id: user.userId,
+            session_id: session.id,
+            status: session.status,
+            order_id: orderId,
+            url: session.url,
+          };
+          const newOrder = new Order(orderData);
+          newOrder
+            .save()
+            .then((savedOrder) => {
+              res.json({
+                message: "Checkout success.",
+                id: session.id,
+                url: session.url,
+              });
+            })
+            .catch((error) => {
+              console.error("Error saving order:", error);
+              res.status(500).json({
+                message: "Error saving order",
+                error: error.message,
+              });
+            });
+        } catch (error) {
+          console.error("Error creating user:", error.message);
+          res.status(400).json({ message: "Error payment" });
+        }
+      } else {
+        res.status(400).json({ message: "user not found" });
+      }
+    } catch (err) {
+      res.status(400).json({ message: "Error payment : " + err });
+    }
   }
 };
 
 const getHistory = async (req, res) => {
-  const { userId } = req.body;
-  try {
-    const order = await Order.find({ user_id: userId });
-    if (order.length > 0) {
-      res.status(200).json({ data: order });
-    } else {
-      res.status(400).json({ status: 400, error: "not found" });
+  const { userId } = req.query; // Change this line to read from query parameters
+  if (!userId) {
+    res.status(400).json({ message: "need user id" });
+  } else {
+    try {
+      const order = await Order.find({ user_id: userId });
+      if (order.length > 0) {
+        res.status(200).json({ data: order });
+      } else {
+        res.status(400).json({ message: "user not found" });
+      }
+    } catch (err) {
+      res.status(400).json({ message: "Error payment : " + err });
     }
-  } catch (err) {
-    res.status(400).json({ status: 400, error: "Error payment" });
   }
 };
 
--- middleware/auth.js
+++ middleware/auth.js
@@ -10,14 +10,14 @@ const verifyToken = (req, res, next) => {
   if (!token) {
     return res
       .status(403)
-      .json({ status: 403, error: "A token is required for authentication" });
+      .json({ message: "A token is required for authentication" });
   }
   try {
     token = token.replace(/^Bearer\s+/, "");
     const decoded = jwt.verify(token, process.env.TOKEN_KEY);
     req.user = decoded;
   } catch (err) {
-    return res.status(401).json({ status: 401, error: "Invalid Token" });
+    return res.status(401).json({ message: "Invalid Token" });
   }
 
   return next();
